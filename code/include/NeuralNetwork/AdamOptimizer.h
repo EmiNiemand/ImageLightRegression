ecd76af124d317e6d053f19fcd80c31718a6ae30
Szymon Habrych <Szymon_PL@live.com>
2024-02-02 22:05:51 +0100

    minor changes

--- code/include/NeuralNetwork/AdamOptimizer.h
+++ code/include/NeuralNetwork/AdamOptimizer.h
@@ -6,8 +6,8 @@
 class AdamOptimizer {
 public:
     float beta1 = 0.9;
-    float beta2 = 0.999;       // Exponential decay rates
-    float learningRate = 0.001;       // Learning rate
+    float beta2 = 0.999;
+    float learningRate = 0.001;
 
 private:
     inline static AdamOptimizer* adamOptimizer = nullptr;
